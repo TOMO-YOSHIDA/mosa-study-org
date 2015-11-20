c3363d8db9e8dd21ffdc553af3f6cbd8470d4406
Tomo Yoshida <yoshida.tomo.1976@gmail.com>
2015-11-20 13:40:45 +0900

    階層変更

--- gulpfile.js
+++ gulpfile.js
@@ -54,14 +54,14 @@ gulp.task('bower', function () {
 	gulp.src(css)
 		.pipe(plum())
 		.pipe(concat('_lib.min.css'))
-		.pipe(gulp.dest('public/temp'));
+		.pipe(gulp.dest('public/mosa-study/temp'));
 
 	var jslib = marge(bowers('js', 'min.js'));
 	jslib.push('src/scripts/**/*.js') // bowerだけど自作JSも含める
 	gulp.src(jslib)
 	.pipe(uglify()) // ハングする
-		.pipe(concat('_lib.min.js'))
-		.pipe(gulp.dest('public/js'));
+		.pipe(concat('lib.min.js'))
+		.pipe(gulp.dest('public/mosa-study/'));
 });
 
 
@@ -75,14 +75,14 @@ gulp.task('less-compile', function () {
 	return gulp.src(lessPath)
 		.pipe(plum())
 		.pipe(less())
-		.pipe(gulp.dest('public/temp'));
+		.pipe(gulp.dest('public/mosa-study/temp'));
 });
 // styledocco
 gulp.task('styledocco', function () {
-	gulp.src('public/temp/**/*.css')
+	gulp.src('public/mosa-study/temp/**/*.css')
 		.pipe(plum())
 		.pipe(styledocco({
-			out: 'public/style-guide',
+			out: 'public/mosa-study/style-guide',
 			name: 'styleguid',
 			verbose: false, // 詳細出力をしない
 			"no-minify": true
@@ -90,11 +90,11 @@ gulp.task('styledocco', function () {
 });
 // css minify
 gulp.task('css-minify', function () {
-	gulp.src('public/temp/**/*.css')
+	gulp.src('public/mosa-study/temp/**/*.css')
 		.pipe(concat(application_name + '.css'))
 		.pipe(autoprefixer())
 		.pipe(minifyCss())
-		.pipe(gulp.dest('public/'));
+		.pipe(gulp.dest('public/mosa-study/'));
 });
 
 // ページ固有のless
@@ -104,7 +104,7 @@ gulp.task('sub-less', function () {
 		.pipe(less())
 		.pipe(autoprefixer())
 		.pipe(minifyCss())
-		.pipe(gulp.dest('public/subcss'));
+		.pipe(gulp.dest('public/mosa-study/subcss'));
 });
 
 //jadeをコンパイルして出力先にコピーする
@@ -112,7 +112,7 @@ gulp.task('jade', function () {
 	gulp.src([jadePath])
 		.pipe(plum())
 		.pipe(jade({ pretty: true }))
-		.pipe(gulp.dest('public/'));
+		.pipe(gulp.dest('public/mosa-study/'));
 });
 
 //ファイルの更新を監視する
--- src/scripts/keisoku.js
+++ src/scripts/keisoku.js
@@ -46,18 +46,56 @@
 			.args 実行時引数
 			.context 実行関数のコンテキスト
 	*/
-	Keisoku.time = function (params) {
+	Keisoku.time = function (title, loopCount, func, args, context) {
+		console.log('time is start!');
+		var params = title;
+		// titleがstringじゃないならオブジェクト
+		if (typeof title === 'string') {
+			params = {
+				title: title,
+				loopCount: loopCount,
+				func: func,
+				args: args,
+				context: context
+			};
+		}
+
+		// 必須チェック
+		var isError = false;
+		['title', 'loopCount', 'func'].forEach(function (r) {
+			if (!params[r]) {
+				console.log('%s undefined.', r);
+				isError = true;
+			}
+		});
+		console.log('is error? :%s', isError);
+		if (isError) return;
+
+		// funcが関数か？
+		if (typeof params.func !== 'function') return console.log('fnc is not Function');
+
+		// argsは配列である必要がある
+		if (Array.isArray(params.args)) {
+			params.args.forEach(function (v, i, arr) {
+				if (!Array.isArray(v)) {
+					arr[i] = [v];
+				}
+			});
+		} else {
+			params.args = [params.args];
+		}
+
+		// ここから計測
 		console.time(params.title);
 		if (Array.isArray(params.args)) {
-
 			for (var i = params.loopCount; i--;) {
 				params.args.forEach(function (a) {
-					params.func(params.context, a);
+					params.func.apply(params.context, a);
 				});
 			}
 		} else {
 			for (var i = params.loopCount; i--;) {
-				params.func(params.context, params.args)
+				params.func.apply(params.context, params.args)
 			}
 		}
 		console.timeEnd(params.title);
