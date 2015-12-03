a9276a07b9a6b67ebf3a99da629e91f7a2405163
Tomo Yoshida <yoshida.tomo.1976@gmail.com>
2015-12-03 10:52:11 +0900

    scriptタグパフォーマンス

--- memo.md
+++ memo.md
@@ -1,9 +1,47 @@
-Javascript勉強会
-コードリーティング、言語仕様解説メインの勉強会をします。
-backboneとかAngularとかTypeScriptとかじゃなく、基礎固め勉強会です。
-P案件から急遽抜けることになったので、スキル引継ぎ会です(業務引き継ぎではない)。
-どなたでも参加できます。
-11月26日(木)19時からオフィスで開催予定です。
 
-内容はこれを元にすすめます。そこそこ知ってる人には初歩的すぎてつまらないと思う。
-http://tomo-yoshida.github.io/mosa-study/
+h3 引数のオブジェクト化
+引数は順番に列挙できるが・・・？
+
+setUserInfo(firstName, lastName) {/*処理略*/}
+
+// ミドルネームを指定できるようにする。引数の順番が違うのでミドルネームを使わなくても影響あり
+setUserInfo(firstName, middleName, lastName) {}
+
+// ミドルネームを指定できるようにする。が、使用しない箇所からの呼び出しには影響しないやりかた
+setUserInfo(firstName, lastName, middleName) {}
+
+// 今度は年齢を入れることになりました。
+setUserInfo(firstName, lastName, middleName, age) {}
+
+// 年齢を入れない場合、生年月日を入れるでもOKになりました
+setUserInfo(firstName, lastName, middleName, age, birthday) {}
+
+こんなことありますよね。
+
+これを引数オブジェクトにすることで万事解決
+setUserInfo(userInfo) {
+	this.firstName = userInfo.firstName;
+	this.middleName = userInfo.middleName || "";
+	this.lastName = userInfo.lastName;
+
+	this.age = userInfo.age || getAge(userInfo.birthday);
+}
+
+ミドルネームを使わない場合、使用者側の変更なし。
+ageを使っていれば、birthdayの追加タイミングでの変更は不要。
+それぞれのチェックも共通関数側で行えるようになります。
+
+欠点としてはparam.firstNameの様に呼び出し側の記述量が増えることでスペルミスのリスクが増えます。
+* TypeScriptを使っていると型チェックができ、インテリセンスで候補が表示されるので安心です。
+
+// 型定義
+interface UserInfo {
+	firstName: string;
+	middleName?: string;
+	lastName: string;
+	age?: number;
+	birthday?: Date;
+}
+
+// 関数側で受ける型を指定
+setUserInfo(userInfo:UserInfo) {}
--- src/scripts/keisoku.js
+++ src/scripts/keisoku.js
@@ -1,10 +1,8 @@
-"use strict";
-
 // IEにはconsole.time関数がないので自作する
 !function (g) {
 
 	var dummyFunction = function () { };
-	var console = g.console || {
+	var console = g['console'] = g.console || {
 		log: dummyFunction,
 		error: dummyFunction,
 		debug: dummyFunction,
@@ -32,72 +30,3 @@
 		};
 	}
 }
-
-// 計測用関数の用意
-!function (g) {
-	var Keisoku = g["Keisoku"] = g.Keisoku || {};
-
-	/*
-		計測用の関数。
-		@param params : object
-			.title 必須：タイトル
-			.loopCount 必須：ループ回数
-			.func 必須：実行関数
-			.args 実行時引数
-			.context 実行関数のコンテキスト
-	*/
-	Keisoku.time = function (title, loopCount, func, args, context) {
-		console.log('time is start!');
-		var params = title;
-		// titleがstringじゃないならオブジェクト
-		if (typeof title === 'string') {
-			params = {
-				title: title,
-				loopCount: loopCount,
-				func: func,
-				args: args,
-				context: context
-			};
-		}
-
-		// 必須チェック
-		var isError = false;
-		['title', 'loopCount', 'func'].forEach(function (r) {
-			if (!params[r]) {
-				console.log('%s undefined.', r);
-				isError = true;
-			}
-		});
-		console.log('is error? :%s', isError);
-		if (isError) return;
-
-		// funcが関数か？
-		if (typeof params.func !== 'function') return console.log('fnc is not Function');
-
-		// argsは配列である必要がある
-		if (Array.isArray(params.args)) {
-			params.args.forEach(function (v, i, arr) {
-				if (!Array.isArray(v)) {
-					arr[i] = [v];
-				}
-			});
-		} else {
-			params.args = [params.args];
-		}
-
-		// ここから計測
-		console.time(params.title);
-		if (Array.isArray(params.args)) {
-			for (var i = params.loopCount; i--;) {
-				params.args.forEach(function (a) {
-					params.func.apply(params.context, a);
-				});
-			}
-		} else {
-			for (var i = params.loopCount; i--;) {
-				params.func.apply(params.context, params.args)
-			}
-		}
-		console.timeEnd(params.title);
-	};
-} (window);
